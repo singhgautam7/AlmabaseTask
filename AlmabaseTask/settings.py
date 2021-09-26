1161ce5b9749fbb42f880b404eeddd89dc0f51f2
Gautam <gautamsingh1997@gmail.com>
2021-09-26 18:40:37 +0530

    settings file change

--- AlmabaseTask/settings.py
+++ AlmabaseTask/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (Path(BASE_DIR).joinpath('static'),)
 
 # Default primary key field type
