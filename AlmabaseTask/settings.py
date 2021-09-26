7a507636e2566af723a971075f79999ffa2e9bf7
Gautam <gautamsingh1997@gmail.com>
2021-09-26 13:22:41 +0530

    settings file change

--- AlmabaseTask/settings.py
+++ AlmabaseTask/settings.py
@@ -82,18 +82,19 @@ WSGI_APPLICATION = 'AlmabaseTask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if os.getenv("DATABASE_URL", "") == "":
+if DEVELOPMENT_MODE is True:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
-else:
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
